59ef5a6ebf4d59d0686b5808b59e610730dc6026
Gael Chabaud-Fouquet <2430057@collegebart.ca>
2025-05-15 13:18:18 -0400

    added more tables, will do the interractions later, need to start tp3 mecaniques de jeu

--- src/database.c
+++ src/database.c
@@ -12,7 +12,7 @@ void database_start(GameData* g){
 const char* create_race_table =
 "CREATE TABLE IF NOT EXISTS Race ("
 "   RaceId INTEGER PRIMARY KEY,"
-"   racename TEXT"
+"   race_name TEXT"
 ");";
 
 const char* create_class_table =
@@ -21,25 +21,45 @@ const char* create_class_table =
 "   class_name TEXT"
 ");";
 
+const char* create_stats_table =
+"CREATE TABLE IF NOT EXISTS Stats ("
+"   StatId INT PRIMARY KEY,"
+"   stat_name TEXT,"
+"   stat_upgrade_cost INT,"
+"   stat_points INT,"
+"   FOREIGN KEY (stat_points) REFERENCES Character(stat_points)"
+");";
+
 const char* create_character_table =
 "CREATE TABLE IF NOT EXISTS Character ("
 "   CharacterId INTEGER PRIMARY KEY,"
 "   gender INT,"// 1==male | 2==female
-"   hairtype INT,"
-"   haircolor INT,"
+"   hair_type INT,"
+"   hair_color INT,"
+"   stat_points INT,"
 "   RaceId INT,"
 "   ClassId INT,"
+"   StatId INT,"
 "   FOREIGN KEY (RaceId) REFERENCES Race(RaceId),"
-"   FOREIGN KEY (ClassId) REFERENCES Classes(ClassId)"
+"   FOREIGN KEY (ClassId) REFERENCES Classes(ClassId),"
+"   FOREIGN KEY (StatId) REFERENCES Stats(StatId)"
 ");";
 
 const char* create_customer_table =
 "CREATE TABLE IF NOT EXISTS Customer ("
 "   CustomerId INTEGER PRIMARY KEY,"
 "   name TEXT,"
-"   email TEXT,"
-"   CharacterId INT,"
-"   FOREIGN KEY (CharacterId) REFERENCES Character(CharacterId)"
+"   email TEXT"
+// "   CharacterId INT,"
+// "   FOREIGN KEY (CharacterId) REFERENCES Character(CharacterId)"
+");";
+
+const char* create_item_table =
+"CREATE TABLE IF NOT EXISTS Item ("
+"   ItemId INT PRIMARY KEY,"
+"   item_name TEXT,"
+"   item_price INT,"
+"   item_availability INT" // 1 == true == available | 0 == false == already bought
 ");";
 
 
@@ -55,6 +75,11 @@ void database_init_tables(GameData* g) {
         return;
     }
     
+    if (sqlite3_exec(g->db, create_stats_table, NULL, NULL, NULL) != SQLITE_OK) {
+        LOG_SQLITE3_ERROR(g->db);
+        return;
+    }
+    
     if (sqlite3_exec(g->db, create_character_table, NULL, NULL, NULL) != SQLITE_OK) {
         LOG_SQLITE3_ERROR(g->db);
         return;
@@ -64,6 +89,12 @@ void database_init_tables(GameData* g) {
         LOG_SQLITE3_ERROR(g->db);
         return;
     }
+
+    if (sqlite3_exec(g->db, create_item_table, NULL, NULL, NULL) != SQLITE_OK) {
+        LOG_SQLITE3_ERROR(g->db);
+        return;
+    }
+
 }
 
 void database_end(GameData* g) {
