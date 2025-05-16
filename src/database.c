4bfe87671400c546bfad457af7c5f2cbb3f8c941
Gael-Chabaud-Fouquet <2430057@bart.ca>
2025-05-16 01:46:02 -0400

    why is it 1:46 am?!? i did not saw the time pass. anyway, added even more tables to the database

--- src/database.c
+++ src/database.c
@@ -15,34 +15,34 @@ const char* create_race_table =
 "   race_name TEXT"
 ");";
 
-const char* create_class_table =
-"CREATE TABLE IF NOT EXISTS Classes ("
-"   ClassId INTEGER PRIMARY KEY,"
-"   class_name TEXT"
+const char* create_socialstatus_table =
+"CREATE TABLE IF NOT EXISTS SocialStatus ("
+"   SocialStatusId INTEGER PRIMARY KEY,"
+"   social_status TEXT"
 ");";
 
-const char* create_stats_table =
-"CREATE TABLE IF NOT EXISTS Stats ("
-"   StatId INT PRIMARY KEY,"
-"   stat_name TEXT,"
-"   stat_upgrade_cost INT,"
-"   stat_points INT,"
-"   FOREIGN KEY (stat_points) REFERENCES Character(stat_points)"
+const char* create_faction_table =
+"CREATE TABLE IF NOT EXISTS Faction ("
+"   FactionId INTEGER PRIMARY KEY,"
+"   faction_name TEXT"
 ");";
 
 const char* create_character_table =
 "CREATE TABLE IF NOT EXISTS Character ("
 "   CharacterId INTEGER PRIMARY KEY,"
+"   name TEXT CHECK (LENGTH(name) <= 16),"
+"   level INT,"
+"   strenght INT,"
+"   stamina INT,"
+"   stealth INT,"
+"   rizz INT," //aka charisma
 "   gender INT,"// 1==male | 2==female
-"   hair_type INT,"
-"   hair_color INT,"
-"   stat_points INT,"
 "   RaceId INT,"
-"   ClassId INT,"
-"   StatId INT,"
+"   SocialStatusId INT,"
+"   FactionId INT,"
 "   FOREIGN KEY (RaceId) REFERENCES Race(RaceId),"
-"   FOREIGN KEY (ClassId) REFERENCES Classes(ClassId),"
-"   FOREIGN KEY (StatId) REFERENCES Stats(StatId)"
+//"   FOREIGN KEY (ClassId) REFERENCES Classes(ClassId),"
+"   FOREIGN KEY (FactionId) REFERENCES Faction(FactionId)"
 ");";
 
 const char* create_customer_table =
@@ -54,14 +54,55 @@ const char* create_customer_table =
 // "   FOREIGN KEY (CharacterId) REFERENCES Character(CharacterId)"
 ");";
 
+
 const char* create_item_table =
 "CREATE TABLE IF NOT EXISTS Item ("
-"   ItemId INT PRIMARY KEY,"
+"   ItemId INTEGER PRIMARY KEY,"
 "   item_name TEXT,"
+"   item_type INT," // 1 == light armor | 2 == heavy armor | 3 == weapon
+"   item_type_type INT," // 1 == helmet | 2 == chest | 3 == legs | 4 == boots | 5 == sword | 6 == bow | etc, etc
+"   item_material INT," // material is color | 1 == blue | 2 == green | etc, etc
 "   item_price INT,"
-"   item_availability INT" // 1 == true == available | 0 == false == already bought
+"   item_availability INT" // 1 == true == available | 0 == false == already bought, thus unavailable
 ");";
 
+const char* insert_race_table =
+"INSERT INTO Race (race_name)"
+"VALUES"
+"   ('Nord'),"
+"   ('Dark_Elf'),"
+"   ('Redguard'),"
+"   ('Orc');";
+
+const char* insert_socialstatus_table =
+"INSERT INTO SocialStatus (social_status)"
+"VALUES"
+"   ('Citizen'),"
+"   ('Merchant'),"
+"   ('Soldier');";
+
+const char* insert_faction_table =
+"INSERT INTO Faction (faction_name)"
+"VALUES"
+"   ('Dawnguard'),"
+"   ('Forsworm'),"
+"   ('BrotherhoodOfSteel'),"
+"   ('Imperials');";
+
+const char* insert_character_table =
+"INSERT INTO Character (name, level, strenght, stamina, stealth, rizz, gender, RaceId, SocialStatusId, FactionId)"
+"VALUES"
+"   ('Placeholder', 1, 1, 1, 1, 1, 1, 1, 1, 1);";
+
+const char* insert_item_table =
+"INSERT INTO Item (item_name, item_type, item_type_type, item_material, item_price, item_availability)"
+"VALUES"
+"   ('helmet', 1, 1, 1, 5, 1),"
+"   ('chest', 2, 2, 1, 8, 1),"
+"   ('legs', 3, 1, 1, 7, 1),"
+"   ('boots', 4, 2, 1, 4, 1),"
+"   ('sword', 5, 5, 1, 2, 1),"
+"   ('shield', 5, 6, 1, 4, 1);";
 
 void database_init_tables(GameData* g) {
     
@@ -70,12 +111,12 @@ void database_init_tables(GameData* g) {
         return;
     }
 
-    if (sqlite3_exec(g->db, create_class_table, NULL, NULL, NULL) != SQLITE_OK) {
+    if (sqlite3_exec(g->db, create_socialstatus_table, NULL, NULL, NULL) != SQLITE_OK) {
         LOG_SQLITE3_ERROR(g->db);
         return;
     }
     
-    if (sqlite3_exec(g->db, create_stats_table, NULL, NULL, NULL) != SQLITE_OK) {
+    if (sqlite3_exec(g->db, create_faction_table, NULL, NULL, NULL) != SQLITE_OK) {
         LOG_SQLITE3_ERROR(g->db);
         return;
     }
@@ -84,17 +125,41 @@ void database_init_tables(GameData* g) {
         LOG_SQLITE3_ERROR(g->db);
         return;
     }
-
+    
     if (sqlite3_exec(g->db, create_customer_table, NULL, NULL, NULL) != SQLITE_OK) {
         LOG_SQLITE3_ERROR(g->db);
         return;
     }
-
+    
     if (sqlite3_exec(g->db, create_item_table, NULL, NULL, NULL) != SQLITE_OK) {
         LOG_SQLITE3_ERROR(g->db);
         return;
     }
+    
+    if (sqlite3_exec(g->db, insert_race_table, NULL, NULL, NULL) != SQLITE_OK) {
+        LOG_SQLITE3_ERROR(g->db);
+        return;
+    }
+    
+    if (sqlite3_exec(g->db, insert_socialstatus_table, NULL, NULL, NULL) != SQLITE_OK) {
+        LOG_SQLITE3_ERROR(g->db);
+        return;
+    }
 
+    if (sqlite3_exec(g->db, insert_faction_table, NULL, NULL, NULL) != SQLITE_OK) {
+        LOG_SQLITE3_ERROR(g->db);
+        return;
+    }
+
+    if (sqlite3_exec(g->db, insert_character_table, NULL, NULL, NULL) != SQLITE_OK) {
+        LOG_SQLITE3_ERROR(g->db);
+        return;
+    }
+    
+    if (sqlite3_exec(g->db, insert_item_table, NULL, NULL, NULL) != SQLITE_OK) {
+        LOG_SQLITE3_ERROR(g->db);
+        return;
+    }
 }
 
 void database_end(GameData* g) {
